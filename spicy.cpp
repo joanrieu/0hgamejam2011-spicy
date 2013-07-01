c614f300315e141cba226259423933493d9e3be0
Joan Rieu <joan.rieu@gmail.com>
2013-07-01 20:28:32 +0200

    Updated the game to the stable SFML 2 API

--- spicy.cpp
+++ spicy.cpp
@@ -3,7 +3,17 @@
 #include <SFML/Audio.hpp>
 
 #define BLOCK 10
-#define V2R(v) sf::FloatRect(v, sf::Vector2f(BLOCK, BLOCK))
+
+sf::FloatRect V2R(const sf::Vector2f& pos) {
+        return sf::FloatRect(pos, sf::Vector2f(BLOCK, BLOCK));
+}
+
+sf::RectangleShape V2R(const sf::Vector2f& pos, const sf::Color& color) {
+        sf::RectangleShape rect(sf::Vector2f(BLOCK, BLOCK));
+        rect.setPosition(pos);
+        rect.setFillColor(color);
+        return rect;
+}
 
 template<typename T>
 struct cmp {
@@ -27,8 +37,8 @@ void updatePhysics(unsigned steps) {
                 pos += spd / 3.f;
 
                 for (BlockSet::const_iterator it = blocks.begin(); it != blocks.end(); ++it)
-                        if (V2R(*it).Intersects(V2R(pos)))
-                                exit(1);
+                        if (V2R(*it).intersects(V2R(pos)))
+                                blocks.erase(it);
 
         }
 
@@ -38,48 +48,53 @@ int main() {
 
         sf::RenderWindow win(sf::VideoMode(800, 600), "Spicy", sf::Style::Default &~ sf::Style::Resize);
 
-        win.ShowMouseCursor(false);
-        win.EnableVerticalSync(true);
+        win.setMouseCursorVisible(false);
+        win.setVerticalSyncEnabled(true);
 
-        sf::Mouse::SetPosition(sf::Vector2i(win.GetWidth() / 2, win.GetHeight() / 2), win);
+        sf::Mouse::setPosition(sf::Vector2i(win.getSize().x / 2, win.getSize().y / 2), win);
 
         blocks.insert(sf::Vector2f(50, 50));
+        blocks.insert(sf::Vector2f(150, 50));
+        blocks.insert(sf::Vector2f(50, 250));
+        blocks.insert(sf::Vector2f(0, 550));
 
-        while (win.IsOpened()) {
+        sf::Clock clock;
+
+        while (win.isOpen()) {
 
                 sf::Event event;
-                while (win.PollEvent(event)) {
+                while (win.pollEvent(event)) {
 
-                        if (event.Type == sf::Event::Closed or (event.Type == sf::Event::KeyPressed and event.Key.Code == sf::Keyboard::Escape))
-                                win.Close();
+                        if (event.type == sf::Event::Closed or (event.type == sf::Event::KeyPressed and event.key.code == sf::Keyboard::Escape))
+                                win.close();
 
-                        else if (event.Type == sf::Event::MouseMoved)
-                                spd = 2.f * sf::Vector2f(static_cast<float>(event.MouseMove.X) / win.GetWidth(), static_cast<float>(event.MouseMove.Y) / win.GetHeight()) - sf::Vector2f(1.f, 1.f);
+                        else if (event.type == sf::Event::MouseMoved)
+                                spd = 2.f * sf::Vector2f(static_cast<float>(event.mouseMove.x) / win.getSize().x, static_cast<float>(event.mouseMove.y) / win.getSize().y) - sf::Vector2f(1.f, 1.f);
 
                 }
 
                 {
-                        const int mx = sf::Mouse::GetPosition(win).x;
-                        const int my = sf::Mouse::GetPosition(win).y;
+                        const int mx = sf::Mouse::getPosition(win).x;
+                        const int my = sf::Mouse::getPosition(win).y;
 
-                        const int nmx = mx < 0 ? 0 : (mx >= win.GetWidth() ? win.GetWidth() : mx);
-                        const int nmy = my < 0 ? 0 : (my >= win.GetHeight() ? win.GetHeight() : my);
+                        const int nmx = mx < 0 ? 0 : (mx >= win.getSize().x ? win.getSize().x : mx);
+                        const int nmy = my < 0 ? 0 : (my >= win.getSize().y ? win.getSize().y : my);
 
-                        sf::Mouse::SetPosition(sf::Vector2i(nmx, nmy), win);
+                        sf::Mouse::setPosition(sf::Vector2i(nmx, nmy), win);
 
                 }
 
-                win.Clear(sf::Color(100, 100, 100));
+                win.clear(sf::Color(100, 100, 100));
 
-                const sf::Uint32 delta = win.GetFrameTime();
+                const sf::Uint32 delta = clock.restart().asMilliseconds();
                 updatePhysics(delta);
 
                 for (BlockSet::const_iterator it = blocks.begin(); it != blocks.end(); ++it)
-                        win.Draw(sf::Shape::Rectangle(V2R(*it - pos), sf::Color(20, 20, 20)));
+                        win.draw(V2R(*it - pos + sf::Vector2f(win.getSize().x / 2, win.getSize().y / 2), sf::Color(20, 20, 20)));
 
-                win.Draw(sf::Shape::Rectangle(V2R(sf::Vector2f(win.GetWidth() / 2, win.GetHeight() / 2)), sf::Color(100, 0, 0)));
+                win.draw(V2R(sf::Vector2f(win.getSize().x / 2, win.getSize().y / 2), sf::Color(100, 0, 0)));
 
-                win.Display();
+                win.display();
 
         }
 
